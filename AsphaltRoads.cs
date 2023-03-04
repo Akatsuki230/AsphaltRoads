cab10eb3bf56b6e7d1cf5bb00dd75d55bf7ed58d
mldkyt <mldkyt@proton.me>
2023-03-04 11:24:16 +0100

    Correct sound effects, remove some unused code, gitignore

--- AsphaltRoads.cs
+++ AsphaltRoads.cs
@@ -12,8 +12,6 @@ public class AsphaltRoads : Mod {
 
     private SettingsCheckBox _dirtRoads, _dirtHouseRoads, _dirtGrass;
     private Material _asphaltMaterial, _pavementMaterial, _pavement2Material;
-    private Material _originalDirtRoads, _originalDirtHouseRoads, _originalDirtHouseGravel, _originalGrass1, _originalGrass2;
-    private PhysicMaterial _originalDirtRoadsPhysicMaterial, _originalDirtHouseRoadsPhysicMaterial, _originalDirtHouseGravelPhysicMaterial, _originalGrass1PhysicMaterial, _originalGrass2PhysicMaterial;
     private PhysicMaterial _asphaltPhysicMaterial;
 
     public override void ModSetup() {
@@ -29,62 +27,49 @@ public class AsphaltRoads : Mod {
         _asphaltPhysicMaterial = bundle.LoadAsset<PhysicMaterial>("Road");
         bundle.Unload(false);
         
-        
         if (_dirtRoads.GetValue()) {
             GameObject road = GameObject.Find("MAP/MESH/TERRAIN_OBJ/Road");
             if (road != null) {
-                // store the original material and physic material
-                _originalDirtRoads = road.GetComponent<MeshRenderer>().material;
-                _originalDirtRoadsPhysicMaterial = road.GetComponent<MeshCollider>().material;
-                // apply the asphalt material and physic material to the dirt road
                 road.GetComponent<MeshRenderer>().material = _asphaltMaterial;
                 road.GetComponent<MeshCollider>().material = _asphaltPhysicMaterial;
+                // set the tag to "Concrete"
+                road.tag = "Concrete";
             }
         }
         
         if (_dirtHouseRoads.GetValue()) {
             GameObject road = GameObject.Find("MAP/MESH/TERRAIN_OBJ/DirtRoad");
             if (road != null) {
-                // store the original material and physic material
-                _originalDirtHouseRoads = road.GetComponent<MeshRenderer>().material;
-                _originalDirtHouseRoadsPhysicMaterial = road.GetComponent<MeshCollider>().material;
-                // apply the asphalt material and physic material to the dirt road
                 road.GetComponent<MeshRenderer>().material = _pavement2Material;
                 road.GetComponent<MeshCollider>().material = _asphaltPhysicMaterial;
+                // set the tag to "Concrete"
+                road.tag = "Concrete";
             }
-            
-            // also apply to MAP/MESH/TERRAIN_OBJ/Gravel
+
             road = GameObject.Find("MAP/MESH/TERRAIN_OBJ/Gravel");
             if (road != null) {
-                // store the original material and physic material
-                _originalDirtHouseGravel = road.GetComponent<MeshRenderer>().material;
-                _originalDirtHouseGravelPhysicMaterial = road.GetComponent<MeshCollider>().material;
-                // apply the asphalt material and physic material to the dirt road
                 road.GetComponent<MeshRenderer>().material = _pavementMaterial;
                 road.GetComponent<MeshCollider>().material = _asphaltPhysicMaterial;
+                // set the tag to "Concrete"
+                road.tag = "Concrete";
             }
         }
         
         if (_dirtGrass.GetValue()) {
             GameObject grass = GameObject.Find("MAP/MESH/TERRAIN_OBJ/Grass1");
             if (grass != null) {
-                // store the original material and physic material
-                _originalGrass1 = grass.GetComponent<MeshRenderer>().material;
-                _originalGrass1PhysicMaterial = grass.GetComponent<MeshCollider>().material;
-                // apply the asphalt material and physic material to the dirt road
                 grass.GetComponent<MeshRenderer>().material = _pavementMaterial;
                 grass.GetComponent<MeshCollider>().material = _asphaltPhysicMaterial;
+                // set the tag to "Concrete"
+                grass.tag = "Concrete";
             }
-            
-            // also apply to MAP/MESH/TERRAIN_OBJ/Grass2
+
             grass = GameObject.Find("MAP/MESH/TERRAIN_OBJ/Grass2");
             if (grass != null) {
-                // store the original material and physic material
-                _originalGrass2 = grass.GetComponent<MeshRenderer>().material;
-                _originalGrass2PhysicMaterial = grass.GetComponent<MeshCollider>().material;
-                // apply the asphalt material and physic material to the dirt road
                 grass.GetComponent<MeshRenderer>().material = _pavementMaterial;
                 grass.GetComponent<MeshCollider>().material = _asphaltPhysicMaterial;
+                // set the tag to "Concrete"
+                grass.tag = "Concrete";
             }
         }
     }
